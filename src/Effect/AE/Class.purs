e1787af76050593ac9aac1fcd6418c5c131c5195
Wei Wei <shazhouke@gmail.com>
2023-12-28 19:45:24 +0800

    Remove unused AE2

--- src/Effect/AE/Class.purs
+++ src/Effect/AE/Class.purs
@@ -6,8 +6,6 @@ import Control.Alternative (class Alternative)
 import Control.Monad.Cont (ContT)
 import Effect (Effect)
 
-type AE2 a = ContT Unit Effect a
-
 class Alternative t <= AsyncTask t where
-  forkTask :: forall a. AE2 a -> Effect (t a)
-  waitTask :: forall a. t a -> AE2 a
+  forkTask :: forall a. ContT Unit Effect a -> Effect (t a)
+  waitTask :: forall a. t a -> ContT Unit Effect a
